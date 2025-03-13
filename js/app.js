1ca83bf5249f384e0359475d78dbba70eb417a5a
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-13 14:44:07 +0100

    feat: create email list with js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-console.log('ciao');
 
 //collegato axios lo uso per richiedere i dati all'API
 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
@@ -6,11 +5,26 @@ axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     let emailList = [];
     for (let i = 0; i < 10; i++){
         const result = response.data.response;
-        // console.log(result);
         emailList.push(result);
     }
     console.log(emailList);
-})
+
+    //salvo in una variabile l'elemento ul che ospiterà la lista delle email
+    const emailListElement = document.getElementById('email-list');
+    console.log(emailListElement);
+    
+    //creo una variabile con una stringa vuota, dove andrò ad aggiungere gli elementi li
+    let emailItems = '';
+    
+    //faccio un ciclo forEach per iterare sull'array creato in precedenza e inserire gli elementi li nella variabile vuota
+    emailList.forEach(email => {
+        emailItems += `<li>${email}</li>`
+    })
+    console.log(emailItems);
+
+    //inserisco gli elementi li creati nell'ul 
+    emailListElement.innerHTML = emailItems;
+}) 
 .catch(error => {
     console.error (error);
 })
