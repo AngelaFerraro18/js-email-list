79a1271db1bf1e7cc6cd6c39353bfffcb354df4d
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-13 16:40:15 +0100

    feat: create event on click btn

--- js/app.js
+++ js/app.js
@@ -25,3 +25,32 @@ for (let i = 0; i < 10; i++) {
         })
 }
 
+//salvo in una variabile l'elemento html button
+const buttonElement = document.getElementById('new-emails');
+
+//creo l'evento per il click sul button
+buttonElement.addEventListener('click',
+    function () {
+        let emailItems = '';
+
+        for (let i = 0; i < 10; i++) {
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
+
+                    //salvo in una variabile il valore che mi interessa (email)
+                    const result = response.data.response;
+
+                    //inserisco gli elementi li nella variabile vuota
+                    emailItems += `<li>${result} <i class="fa-regular fa-envelope"></i><i class="fa-regular fa-envelope-open"></i></li>`
+
+                    //inserisco gli elementi li creati nell'ul 
+                    emailListElement.innerHTML = emailItems;
+
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+        }
+    })
+
