7f7c4debe447926f8645e56ea4f96de3db8fc30e
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-13 14:23:18 +0100

    feat: create 10 random emails using for cicle

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 console.log('ciao');
 
+//collegato axios lo uso per richiedere i dati all'API
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(response => {
+    let emailList = [];
+    for (let i = 0; i < 10; i++){
+        const result = response.data.response;
+        // console.log(result);
+        emailList.push(result);
+    }
+    console.log(emailList);
+})
+.catch(error => {
+    console.error (error);
+})
