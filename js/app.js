b943475f881b65791fc3ce39a3f29405591550d7
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-13 17:43:09 +0100

    feat: using fetch to generate 10 random email with btn click

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ const emailListElement = document.getElementById('email-list');
 //creo una variabile con una stringa vuota, dove andrò ad aggiungere gli elementi li
 let emailItems = '';
 
+//salvo in una variabile l'elemento contenitore della lista ul
+const emailContainerElement = document.getElementById('email-container');
+
+//salvo in una variabile l'elemento html button
+const buttonElement = document.getElementById('new-emails');
+
 //collegato axios lo uso per richiedere i dati all'API e faccio un ciclo for per generare 10 email randomiche
 for (let i = 0; i < 10; i++) {
 
@@ -21,12 +27,12 @@ for (let i = 0; i < 10; i++) {
 
         })
         .catch(error => {
-            console.error(error);
+
+            emailContainerElement.innerHTML = '<p>Ops, qualcosa è andato storto... <i class="fa-regular fa-face-frown"></i></p>';
+            buttonElement.classList.add('d-none');
+
         })
 }
-
-//salvo in una variabile l'elemento html button
-const buttonElement = document.getElementById('new-emails');
 
 //creo l'evento per il click sul button
 buttonElement.addEventListener('click',
@@ -34,22 +40,18 @@ buttonElement.addEventListener('click',
         let emailItems = '';
 
         for (let i = 0; i < 10; i++) {
-
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            //uso fetch per generare altre 10 email random 
+            fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => response.json())
                 .then(response => {
 
-                    //salvo in una variabile il valore che mi interessa (email)
-                    const result = response.data.response;
+                    const result = response.response;
 
                     //inserisco gli elementi li nella variabile vuota
                     emailItems += `<li>${result} <i class="fa-regular fa-envelope"></i><i class="fa-regular fa-envelope-open"></i></li>`
 
                     //inserisco gli elementi li creati nell'ul 
                     emailListElement.innerHTML = emailItems;
-
-                })
-                .catch(error => {
-                    console.error(error);
                 })
         }
     })
